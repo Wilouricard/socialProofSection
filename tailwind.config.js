30a9c5b9cff640153261216065badd1b0bac68a1
Nam Tang <nam.tang@live.fr>
2022-10-08 16:01:49 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {},
+    colors: {
+      'Very-Dark-Magenta': 'hsl(300, 43%, 22%)',
+      'Soft-Pink': 'hsl(333, 80%, 67%)',
+      'Dark-Grayish-Magenta': 'hsl(303, 10%, 53%)',
+      'Light-Grayish-Magenta': 'hsl(300, 24%, 96%)',
+      'White': 'hsl(0, 0%, 100%)'
+    },
+  },
+  plugins: [],
+}
